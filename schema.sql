fc817b70deae3ebb170edf5f0fe25df10065cbd9
ifzyy <johnsonnifemi8@gmail.com>
2022-09-16 09:26:45 +0100

    fixed reviewer issue

--- schema.sql
+++ schema.sql
@@ -12,13 +12,13 @@ CREATE TABLE animals(
 
 CREATE TABLE owners(
     id SERIAL PRIMARY KEY,
-    full_name text,
+    full_name VARCHAR,
     age integer
 );
 
 CREATE TABLE species(
     id SERIAL PRIMARY KEY,
-    name text
+    name VARCHAR
 );
 ALTER TABLE animals ADD COLUMN species_id INT, ADD FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
 ALTER TABLE animals ADD COLUMN owner_id INT, ADD FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE;
